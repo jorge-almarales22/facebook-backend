9f5e5c27f205ab088ebbb8ce62bf3252c43d2a8b
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-21 18:56:21 -0500

    upload post connected 100%

--- sockets/controller.socket.js
+++ sockets/controller.socket.js
@@ -1,11 +1,12 @@
 export const socketController = (socket) => {
-    console.log("a user connected", socket.id);
-
-    socket.on('disconnect', () => {
-        console.log("user disconnected", socket.id);
-    })
-    socket.on('mensaje_cliente', (mensaje) => {
-        console.log('Mensaje del cliente:', mensaje);
+    
+    socket.on('upload_post', (mensaje) => {
+        console.log('EVENT UPLOAD_POST', mensaje);
+        socket.broadcast.emit('uploaded_post', socket.id);
     });
-      
+    
+    // console.log("a user connected", socket.id);
+    // socket.on('disconnect', () => {
+    //     console.log("user disconnected", socket.id);
+    // })
 }
